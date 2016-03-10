68d47abc196b92834165a6e0e86213bf2348d7fe
mengyangyang <853773190@qq.com>
2016-03-10 10:44:42 +0800

    test

---
+++ src/mengyyGitTest/GitTest.java
@@ -0,0 +1,10 @@
+package mengyyGitTest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello MengYangyang!!!");
+	}
+
+}
